300b78155d17dda78c6ccd7f9be8c2156e886cb5
Suhail Doshi <digitalwarfare@gmail.com>
2018-11-16 02:13:17 -0500

    updated model

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,4 @@
 export Torch_DIR=../libtorch/share/cmake/Torch
 export Caffe2_DIR=../libtorch/share/cmake/Caffe2
 cmake -DCMAKE_PREFIX_PATH=../libtorch/ ..
-#cmake -DCMAKE_PREFIX_PATH=../libtorch/ -DCMAKE_BUILD_TYPE=release -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DARCHIVE_INSTALL_DIR=. -G "Unix Makefiles" ..
 make
